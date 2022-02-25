a64014505922c77845e9bd595e501f93c3070673
Noreddine Hakkaoui <nhakkaou@e2r4p14.1337.ma>
2022-02-25 21:18:28 +0100

    Script Agent

--- p1/script.sh
+++ p1/script.sh
@@ -2,5 +2,5 @@
 
 echo "**** Begin installing k3s"
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--advertise-address=192.168.42.110" sh -
-sudo cat /var/lib/rancher/k3s/server/token > /vagrant/token
+cp /var/lib/rancher/k3s/server/token /vagrant_shared
 echo "**** End installing k3s"
--- p1/script2.sh
+++ p1/script2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 echo "**** Begin installing k3s"
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_TOKEN="./token" sh -
+export K3S_TOKEN_FILE=/vagrant_shared/token
+export K3S_URL=https://192.168.42.110:6443
+export INSTALL_K3S_EXEC="--flannel-iface=eth1"
+curl -sfL https://get.k3s.io | sh -
 echo "**** End installing k3s"
