096c8e721400c5038509cbd1337a637ddd722823
Noreddine Hakkaoui <nhakkaou@e2r2p9.1337.ma>
2022-02-26 18:26:25 +0100

    Fix Link folder

--- p1/script2.sh
+++ p1/script2.sh
@@ -2,6 +2,7 @@
 
 echo "**** Begin installing k3s"
 export K3S_TOKEN_FILE=/vagrant_shared/token
+echo $K3S_TOKEN_FILE
 export K3S_URL=https://192.168.42.110:6443
 export INSTALL_K3S_EXEC="--flannel-iface=eth1"
 curl -sfL https://get.k3s.io | sh -
