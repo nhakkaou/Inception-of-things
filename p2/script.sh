7597bc2e56802f0e800255682c34705a0a5cbc3b
Noreddine Hakkaoui <nhakkaou@e2r2p9.1337.ma>
2022-02-27 22:04:58 +0100

    Create developement files

--- p2/script.sh
+++ p2/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "**** Begin installing k3s"
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--bind-address=192.168.42.110  --node-ip=192.168.42.110  --tls-san $(hostname) --advertise-address=192.168.42.110" sh -
+echo "Config Deployement k3s"
 echo "**** End installing k3s"
