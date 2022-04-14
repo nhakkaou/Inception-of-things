85f0b7b8efa1e66ce5919e89980650dc96a6d4c3
Noreddine Hakkaoui <nhakkaou@e2r7p8.1337.ma>
2022-04-14 17:56:32 +0000

    Fix First part worker

--- p1/script.sh
+++ p1/script.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-
-echo "**** Begin installing k3s"
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="-flannel-iface eth1" sh -
-cp /var/lib/rancher/k3s/server/token /vagrant_shared
-echo "**** End installing k3s"
+echo "******* Begin installing k3s *********"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=192.168.42.110  --node-ip=192.168.42.110   --advertise-address=192.168.42.110"
+curl -sfL https://get.k3s.io | sh -
+sudo cp /var/lib/rancher/k3s/server/token /vagrant_shared/
+echo "********end*******"
--- p1/script2.sh
+++ p1/script2.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-
 echo "**** Begin installing k3s"
-export K3S_TOKEN_FILE=/vagrant_shared/token
-echo $K3S_TOKEN_FILE
-export K3S_URL=https://192.168.42.110:6443
-export INSTALL_K3S_EXEC="--flannel-iface=eth1"
+export INSTALL_K3S_EXEC="agent --server  https://192.168.42.110:6443 --token $(sudo cat /vagrant_shared/token) --node-ip=192.168.42.111"
 curl -sfL https://get.k3s.io | sh -
-echo "**** End installing k3s"
+echo "********end*******"
